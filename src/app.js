74ccee411877322b7b5284de872ce1eaed86a48b
Matan Kadosh <kadosh10001@gmail.com>
2020-04-22 19:24:28 +0300

    * add modifyDbCluster method * change all password and keys to vault * use settings for auth

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const aws = require("aws-sdk");
 
-function handleInstance(type){
-    return function(action){
-        return new Promise((resolve,reject)=>{
-            aws.config.update({
-                region: action.params.REGION,
-                accessKeyId: action.params.AWS_ACCESS_KEY_ID,
-                secretAccessKey: action.params.AWS_SECRET_ACCESS_KEY
-            });
+function _getRds(action,settings){
+    return new aws.RDS({
+        region: action.params.REGION,
+        accessKeyId: action.params.AWS_ACCESS_KEY_ID || settings.AWS_ACCESS_KEY_ID,
+        secretAccessKey: action.params.AWS_SECRET_ACCESS_KEY || settings.AWS_SECRET_ACCESS_KEY
+    })
+}
 
-            let rds = new aws.RDS();
+function handleInstance(type){
+    return function(action, settings){
+        return new Promise((resolve,reject)=>{
+            
+            const rds = _getRds(action,settings);
 
             function callback(err, result){
                 if (err) return reject(err);
@@ -52,7 +55,47 @@ function handleInstance(type){
     }
 }
 
+function modifyDBCluster(action,settings){
+    return new Promise((resolve,reject)=>{
+        const rds = _getRds(action,settings);
+
+        const params = {
+            DBClusterIdentifier : action.params.dbClusterIdentifier
+        }
+
+        if(action.params.scalingConfigurationMinCapacity || action.params.scalingConfigurationMaxCapacity){
+            params.ScalingConfiguration = {
+                MinCapacity: action.params.scalingConfigurationMinCapacity,
+                MaxCapacity: action.params.scalingConfigurationMaxCapacity
+            }
+        }
+
+        if(action.params.masterUserPassword){
+            params.MasterUserPassword = action.params.masterUserPassword;
+        }
+
+        if(action.params.domain){
+            params.Domain = action.params.domain;
+        }
+
+        if(action.params.preferredBackupWindow){
+            params.PreferredBackupWindow = action.params.preferredBackupWindow;
+        }
+
+        if(action.params.preferredMaintenanceWindow){
+            params.PreferredMaintenanceWindow = action.params.preferredMaintenanceWindow;
+        }
+
+        rds.modifyDBCluster(params, (err,result)=>{
+            if(err) return reject(err);
+            resolve(result)
+        })
+    })
+    
+}
+
 module.exports = {
     createDbInstance: handleInstance('new'),
-    deleteDbInstances: handleInstance('delete')
+    deleteDbInstances: handleInstance('delete'),
+    modifyDBCluster : modifyDBCluster
 };
